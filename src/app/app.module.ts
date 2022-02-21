704c54b0529a88835e5f9f7487089e0e695fe4f5
rorofino10 <rorofino10@gmail.com>
2022-02-21 18:16:09 -0300

    Positions and Gizmo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
@@ -13,6 +13,7 @@ import { ViewerComponent } from './viewer/viewer.component';
     ViewerComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule
   ],
--- src/app/viewer/viewer.component.ts
+++ src/app/viewer/viewer.component.ts
@@ -1,7 +1,9 @@
 import { Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core' 
+import { AbstractMesh, AxisDragGizmo, GizmoManager, GUID, Nullable } from '@babylonjs/core'
 import { ArcRotateCamera, Color3, HemisphericLight, StandardMaterial, Texture, Viewport } from '@babylonjs/core' 
 import { Color4, Engine, FreeCamera, Light, Mesh, MeshBuilder, Scene, SceneLoader, Tools, UniversalCamera, Vector3 } from '@babylonjs/core' 
 import "@babylonjs/loaders" 
+
 
 @Component({
   selector: 'app-viewer',
@@ -16,14 +18,25 @@ export class ViewerComponent implements OnInit {
   private light: HemisphericLight | undefined 
   private camera: ArcRotateCamera | undefined
   private model: SceneLoader | any
-  public open: boolean = false
+  public open: boolean = true
   private meshes: any 
-
+  private gizmoManager: GizmoManager | undefined
+  public selectedMesh: Mesh | any
+  public selectedMeshpositionx: any
+  public selectedMeshpositiony: any
+  public selectedMeshpositionz: any
+  public selectedMeshrotatex: any
+  public selectedMeshrotatey: any
+  public selectedMeshrotatez: any
+  public selectedMeshscalex: any
+  public selectedMeshscaley: any
+  public selectedMeshscalez: any
   @ViewChild('renderCanvas', { static: true }) renderCanvas!: ElementRef 
-
+  @ViewChild('meshprop', {static: true}) meshprop!: ElementRef
   constructor() {
 
   }
+  
 
   ngOnInit(): void {
     this.modelEngine() 
@@ -41,31 +54,79 @@ export class ViewerComponent implements OnInit {
     this.camera.attachControl(this.renderCanvas.nativeElement, true) 
     this.camera.wheelPrecision = 5 
     this.camera.lowerRadiusLimit = 2 
-    this.camera.upperRadiusLimit = 40 
+    this.camera.upperRadiusLimit = 80 
     this.camera.panningSensibility = 1950
     
 
-    
-    // this.camera.alpha = -Math.PI/2 
-
-    // this.camera.beta = 0.01 
-
-    // this.camera.radius = 85 
-
-    // this.camera.target = new Vector3(0, 0, 0) 
-
-
     this.light = new HemisphericLight("hemi", new Vector3(0, 1, 0), this.scene) 
-    // this.light.groundColor = new Color3.Gray 
+    this.gizmoManager = new GizmoManager(this.scene)
+    this.gizmoManager.clearGizmoOnEmptyPointerEvent = true;
+    this.gizmoManager.positionGizmoEnabled = true;
+    this.gizmoManager.rotationGizmoEnabled = false;
+    this.gizmoManager.scaleGizmoEnabled = false;
+    this.gizmoManager.boundingBoxGizmoEnabled = false;
+    this.gizmoManager.usePointerToAttachGizmos = true;
+    this.gizmoManager.onAttachedToMeshObservable.add((evt) => {
+      this.selectedMesh = evt
+      if (this.selectedMesh) {
+        this.selectedMeshpositionx = this.selectedMesh.position.x.toFixed(2)
+        this.selectedMeshpositiony = this.selectedMesh.position.y.toFixed(2)
+        this.selectedMeshpositionz = this.selectedMesh.position.z.toFixed(2)
 
-    // this.scene.createDefaultLight() 
+        this.selectedMeshrotatex = this.selectedMesh.rotation.x.toFixed(2)
+        this.selectedMeshrotatey = this.selectedMesh.rotation.y.toFixed(2)
+        this.selectedMeshrotatez = this.selectedMesh.rotation.z.toFixed(2)
 
-    this.model = SceneLoader.ImportMesh("", "./assets/", "planocomotuhermana.gltf", this.scene, function(meshes) {
-      self.meshes = meshes
-      if (meshes) {
-        meshes[1].material!.wireframe = false
+        this.selectedMeshscalex = this.selectedMesh.scaling.x.toFixed(2)
+        this.selectedMeshscaley = this.selectedMesh.scaling.y.toFixed(2)
+        this.selectedMeshscalez = this.selectedMesh.scaling.z.toFixed(2)
       }
+    })
+    this.gizmoManager.gizmos.positionGizmo?.onDragEndObservable.add((evt) => {
+      console.log(this.gizmoManager!.gizmos.positionGizmo);
+      if (this.selectedMesh) {
+        this.selectedMeshpositionx = this.selectedMesh.position.x.toFixed(2)
+        this.selectedMeshpositiony = this.selectedMesh.position.y.toFixed(2)
+        this.selectedMeshpositionz = this.selectedMesh.position.z.toFixed(2)
+
+      }
+    })
+    this.gizmoManager.gizmos.rotationGizmo?.onDragEndObservable.add((evt) => {
+      this.selectedMeshrotatex = this.selectedMesh.rotation.x.toFixed(2)
+      this.selectedMeshrotatey = this.selectedMesh.rotation.y.toFixed(2)
+      this.selectedMeshrotatez = this.selectedMesh.rotation.z.toFixed(2)
+    })
+    this.gizmoManager.gizmos.scaleGizmo?.onDragEndObservable.add((evt) => {
+      this.selectedMeshscalex = this.selectedMesh.scaling.x.toFixed(2)
+      this.selectedMeshscaley = this.selectedMesh.scaling.y.toFixed(2)
+      this.selectedMeshscalez = this.selectedMesh.scaling.z.toFixed(2)
+    })
+    
+
+
+    SceneLoader.ImportMesh("", "./assets/", "planocomotuhermana.gltf", this.scene, function(meshes) {
+      self.meshes = meshes
+      meshes.forEach(mesh => {
+        mesh.isPickable = false
+      });
     }) 
+    // const extintor = SceneLoader.ImportMesh("", "./assets/", "extintor.gltf", this.scene, function(meshes) {
+    //   if (meshes) {
+    //     console.log(meshes[0]);
+    //     meshes[0].scaling = new Vector3(1, 1,-1)
+    //   }
+    // }) 
+
+
+    // SceneLoader.ImportMesh("", "./assets/", "extintor.gltf", this.scene, function(meshes) {
+    //   self.meshes = meshes
+    // }) 
+    // SceneLoader.ImportMesh("", "https://www.babylonjs.com/Assets/Yeti/glTF/", "Yeti_Idle.gltf", this.scene, function(meshes){
+    //   if (meshes) {
+    //     console.log(meshes[0]);
+    //     meshes[0].scaling = new Vector3(0.35, 0.35,-0.35)
+    //   }
+    // })
 
     // this.svg = MeshBuilder.CreateGround("plano", {width: 6, height: 6}, this.scene) 
     // this.svg.rotation.x = Tools.ToRadians(270) 
@@ -78,6 +139,7 @@ export class ViewerComponent implements OnInit {
 
     // this.svg.material = svgmaterial 
     this.engine.runRenderLoop(() => {
+
       if (this.camera) {
         // console.log(this.camera.alpha) 
         // console.log(this.camera.radius) 
@@ -85,7 +147,7 @@ export class ViewerComponent implements OnInit {
         // console.log(this.camera.target) 
         this.camera.panningSensibility = (this.camera.radius-2)/38*-1850+1950
         // console.log(this.camera.panningSensibility)
-
+        
         if (this.camera.target.z <= -20 || this.camera.target.z >=8) {
           this.camera.target.z = 0
         }
@@ -108,6 +170,14 @@ export class ViewerComponent implements OnInit {
     };      
     })
 
+  }
+
+
+  addExtintor(){
+    SceneLoader.ImportMesh("", "./assets/", "extintor.gltf", this.scene)
+  }
+  addAxe(){
+    SceneLoader.ImportMesh("", "./assets/", "axe.gltf", this.scene)
   }
 
   toggleWireframe(){
@@ -144,6 +214,46 @@ export class ViewerComponent implements OnInit {
   zoomOut(){
     if (this.camera) {
       this.camera.radius = this.camera.radius + 5 
+    }
+  }
+
+  updateMeshPosition(){
+    this.selectedMesh.position.x = this.selectedMeshpositionx
+    this.selectedMesh.position.y = this.selectedMeshpositiony
+    this.selectedMesh.position.z = this.selectedMeshpositionz
+
+    this.selectedMesh.rotation.x = this.selectedMeshrotatex  
+    this.selectedMesh.rotation.y = this.selectedMeshrotatey  
+    this.selectedMesh.rotation.z = this.selectedMeshrotatez  
+
+    this.selectedMesh.scaling.x = this.selectedMeshscalex
+    this.selectedMesh.scaling.y = this.selectedMeshscaley 
+    this.selectedMesh.scaling.z = this.selectedMeshscalez
+    
+  }
+
+  moveGizmo(){
+    if (this.gizmoManager) {
+      this.gizmoManager.positionGizmoEnabled = true;
+      this.gizmoManager.rotationGizmoEnabled = false;
+      this.gizmoManager.scaleGizmoEnabled = false;
+      this.gizmoManager.boundingBoxGizmoEnabled = false;
+    }
+  }
+  rotateGizmo(){
+    if (this.gizmoManager) {
+      this.gizmoManager.positionGizmoEnabled = false;
+      this.gizmoManager.rotationGizmoEnabled = true;
+      this.gizmoManager.scaleGizmoEnabled = false;
+      this.gizmoManager.boundingBoxGizmoEnabled = false;
+    }
+  }
+  scaleGizmo(){
+    if (this.gizmoManager) {
+      this.gizmoManager.positionGizmoEnabled = false;
+      this.gizmoManager.rotationGizmoEnabled = false;
+      this.gizmoManager.scaleGizmoEnabled = true;
+      this.gizmoManager.boundingBoxGizmoEnabled = false;
     }
   }
 }
